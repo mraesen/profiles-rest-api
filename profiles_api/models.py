069b72c88ca7722b24fc960e487d62ac4f960c8e
Raesen <rmatut2@wgu.edu>
2025-01-06 14:50:27 -0600

    Enabled Django admin for user profiles model

--- profiles_api/models.py
+++ profiles_api/models.py
@@ -28,7 +28,7 @@ class UserProfileManager(BaseUserManager):
         user.is_staff = True
         user.save(using=self._db)
 
-        return user 
+        return user
 
 
 class UserProfile(AbstractBaseUser, PermissionsMixin):
@@ -41,7 +41,7 @@ class UserProfile(AbstractBaseUser, PermissionsMixin):
     objects = UserProfileManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELD = ['name']
+    REQUIRED_FIELDS = ['name']
 
     def get_full_name(self):
         """Retrieve full name of user"""
